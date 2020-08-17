9639b0d53de5a584a10b84c41eb1bc9eefdb72c8
xiaoao <745133621@qq.com>
2020-08-17 22:55:22 +0800

    Initial commit

---
+++ src/main/java/com/A.java
@@ -0,0 +1,13 @@
+package com;
+
+import jdk.nashorn.internal.objects.annotations.Setter;
+
+//@Data
+public class A {
+
+    private String name;
+
+    public static void main(String[] args) {
+        System.out.println("面对疾风吧");
+    }
+}
